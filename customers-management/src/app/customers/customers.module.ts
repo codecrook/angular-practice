64e36eaf9f3554ac1f92b0dcae40edc7cbfc7619
Deeptiman Mallick <hello@codecrook.co>
2021-01-11 22:01:54 +0530

    (1) Learned to create new project, componets and module
    - Created a new project using Angular CLI
    - Created Components manually without `ng g c`
    - Created Modules and learned about imports, exports and bootstrap

---
+++ customers-management/src/app/customers/customers.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+    declarations: [CustomersComponent],
+    imports: [CommonModule],
+    exports: [CustomersComponent]
+})
+export class CustomersModule { }
